f0e77c09ba7dc755b35fe7d153c9e07825e89d26
waseemarchiwiz <waseem@archiwiz.com>
2024-10-18 12:23:02 +0500

    added the category tabs form and table

--- src/menu-items/index.jsx
+++ src/menu-items/index.jsx
@@ -1,13 +1,19 @@
 import { useSelector } from "react-redux";
 import dashboard from "./dashboard";
 import OperatorMenu from "./operator";
+import { useEffect, useState } from "react";
 
 const useMenuItems = () => {
   const userData = useSelector((state) => state.auth.user);
+  const [user, setUser] = useState({});
 
-  // Retrieve and parse the user data from localStorage if userData is not available
-  const storedUser = localStorage.getItem("auth");
-  const user = userData || (storedUser && JSON.parse(storedUser).data);
+  useEffect(() => {
+    // Retrieve and parse the user data from localStorage if userData is not available
+    const storedUser = localStorage.getItem("auth");
+    const user = userData || (storedUser && JSON.parse(storedUser));
+    setUser(user);
+    console.log("I am user in index routes page: ", user);
+  }, [userData]);
 
   console.log("I am user in index routes page: ", user);
 
